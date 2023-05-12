9b6d1a3ffb796aa7cf596173cb962624aa869c2c
Carissa03-boop <125882806+Carissa03-boop@users.noreply.github.com>
2023-05-12 17:00:28 +0200

    homework

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let susan = 12;
+let timmy = 20;
+let josh = 14;
+let win = false
+
+let teamST = susan + timmy; 
+let victory = josh / 2;
+let answer = teamST + victory;
+
+console.log(victory);
+console.log(teamST);
+console.log(answer);
+console.log(win)
